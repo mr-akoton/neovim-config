b5911ac1e03848f29f6184c8ce78029821235121
mr-akoton <rakotonirina03mirado@gmail.com>
2025-07-10 10:06:36 +0300

    add: trying to install c/c++ extension

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.options")
-require("config.keymaps")
-require("config.lazy")
+require("config")
 require("plugins.colorscheme")
 
 -- vim.cmd.colorscheme("tokyonight")
